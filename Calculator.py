e1a897c6a6e66fce2a9d97dad794d87f14a28909
voidnologo <(none)>
2016-10-22 21:52:38 -0500

    Alternate implementation

--- Calculator.py
+++ Calculator.py
@@ -1,54 +1,87 @@
 from math import sqrt
 
-while True:
-    print("Welcome to the calculator!")
-    print("For addition enter add")
-    print("For subtraction enter subtract")
-    print("For multiplication enter multiply")
-    print("For division enter divide")
-    print("For squaring enter square")
-    print("For square-root enter root")
-    print("To quit enter quit")
-    option = input("Please enter an option: ")
+prompt = """
+Welcome to the calculator!
+         Operation : Command
+         -------------------
+          Addition : add
+       Subtraction : subtract
+    Multiplication : multiply
+          Division : divide
+          Squaring : square
+       Square-root : root
 
+           To quit : quit
+"""
+
+
+separator = "=" * 35
+
+
+def get_two_numbers():
+  number1 = float(input("\t Enter the first number > "))
+  number2 = float(input("\tEnter the second number > "))
+  return number1, number2
+
+
+def get_one_number():
+  return float(input("Enter a number > "))
+
+
+def add():
+    number1, number2 = get_two_numbers()
+    print('The answer is {}'.format(number1 + number2))
+    print(separator)
+
+
+def subtract():
+    print("Remember order matters for subtraction! ")
+    number1, number2 = get_two_numbers()
+    print('The answer is {}'.format(number1 - number2))
+    print(separator)
+
+
+def multiply():
+    number1, number2 = get_two_numbers()
+    print('The answer is {}'.format(number1 * number2))
+    print(separator)
+
+
+def divide():
+    print("Remember order matters for division! ")
+    number1, number2 = get_two_numbers()
+    print('The answer is {}'.format(number1 / number2))
+    print(separator)
+
+
+def square():
+    number = get_one_number()
+    print('The answer is {}'.format(number * number))
+    print(separator)
+
+
+def root():
+    number = get_one_number()
+    print('The answer is {}'.format(sqrt(number)))
+    print(separator)
+
+
+options = {
+    'add': add,
+    'subtract': subtract,
+    'multiply': multiply,
+    'divide': divide,
+    'square': square,
+    'root': root
+}
+
+
+while True:
+    print(prompt)
+    option = input("Please enter an option: ")
     if option == "quit":
-          break
-    elif option == "add":
-          number1 = float(input("Enter the first number. "))
-          number2 = float(input("Enter the second number. "))
-          answer = str(number1 + number2)
-          print("The answer is " + answer)
-          print("=" * 35)
-    elif option == "subtract":
-          print("Remember order matters for subtraction! ")
-          number1 = float(input("Enter the first number. "))
-          number2 = float(input("Enter the second number. "))
-          answer = str(number1 - number2)
-          print("The answer is " + answer)
-          print("=" * 35)
-    elif option == "multiply":
-          number1 = float(input("Enter the first number. "))
-          number2 = float(input("Enter the second number. "))
-          answer = str(number1 * number2)
-          print("The answer is " + answer)
-          print("=" * 35)
-    elif option == "divide":
-          print("Remember order matters for division! ")
-          number1 = float(input("Enter the first number (dividend) "))
-          number2 = float(input("Enter the second number (divisor) "))
-          answer = str(number1 / number2)
-          print("The answer is " + answer)
-          print("=" * 35)
-    elif option == "square":
-          number1 = float(input("Enter a number. "))
-          answer = str(number1 * number1)
-          print("The answer is " + answer)
-          print("=" * 35)
-    elif option == "root":
-          number1 = float(input("Enter a number. "))
-          answer = str(sqrt(number1))
-          print("The answer is " + answer)
-          print("=" * 35)
-    else:
-          print("Unknown option!")
-          print("=" * 35)
+        break
+    try:
+        options[option]()
+    except KeyError:
+        print('Invalid operation.')
